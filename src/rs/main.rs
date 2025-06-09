7582c6b435789374869202c3c507ab3a6f4da794
Brendon Bown <brendon@careweather.com>
2025-06-09 10:38:00 -0600

    [rust-cli] Implement basic AST parser

--- src/rs/main.rs
+++ src/rs/main.rs
@@ -1,3 +1,32 @@
+pub mod cli;
+
+use clap::Parser;
+use oneil::parser::{self, Config, Span};
+use std::{fs::File, io::Read};
+
+use crate::cli::{Cli, Commands, DevCommands};
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Dev { command } => match command {
+            DevCommands::PrintAst { file } => {
+                let mut file_content = String::new();
+                File::open(&file)
+                    .expect("Unable to open file")
+                    .read_to_string(&mut file_content)
+                    .expect("Unable to read file");
+
+                let input = Span::new_extra(&file_content, Config::default());
+                // Assuming there's a function to parse the file content into an AST
+                let ast = parser::model::parse(input);
+
+                match ast {
+                    Ok((_rest, ast)) => println!("{:?}", ast),
+                    Err(e) => eprintln!("Error printing AST: {:?}", e),
+                }
+            }
+        },
+    }
 }
