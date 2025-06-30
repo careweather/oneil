119d28c3ec984ec163c001b8e6b7f1734a67e289
Brendon Bown <brendon@careweather.com>
2025-06-30 11:37:32 -0600

    [vscode] Add entrypoint for extension

---
+++ vscode/src/extension.ts
@@ -0,0 +1,7 @@
+import * as vscode from "vscode";
+
+export function activate(context: vscode.ExtensionContext) {
+    console.log("Oneil extension is now active!");
+}
+
+export function deactivate() { }
