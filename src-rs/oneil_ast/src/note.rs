1012282521c614fa18d012cbfed1f4308e5902db
Brendon Bown <brendon@careweather.com>
2025-07-14 16:22:59 -0600

    [rust-ast] Update notes to use nodes

--- src-rs/oneil_ast/src/note.rs
+++ src-rs/oneil_ast/src/note.rs
@@ -1,7 +1,21 @@
+use crate::node::Node;
+
 /// A documentation note in the AST
 ///
 /// Notes can be attached to various AST nodes to provide documentation,
 /// explanations, or other comments. They can be either single-line notes
 /// starting with `~` or multi-line notes delimited by `~~~`.
 #[derive(Debug, Clone, PartialEq)]
-pub struct Note(pub String);
+pub struct Note(String);
+
+impl Note {
+    pub fn new(value: String) -> Self {
+        Self(value)
+    }
+
+    pub fn value(&self) -> &str {
+        &self.0
+    }
+}
+
+pub type NoteNode = Node<Note>;
