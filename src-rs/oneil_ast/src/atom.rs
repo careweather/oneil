f0c6d680ef75ebf29438a892f2f09e0d7865aabb
Brendon Bown <brendon@careweather.com>
2025-07-14 16:30:01 -0600

    [rust-ast] Update parameter to use nodes

--- src-rs/oneil_ast/src/atom.rs
+++ src-rs/oneil_ast/src/atom.rs
@@ -16,6 +16,21 @@ impl Identifier {
 pub type IdentifierNode = Node<Identifier>;
 
 #[derive(Debug, Clone, PartialEq)]
+pub struct Label(String);
+
+impl Label {
+    pub fn new(value: String) -> Self {
+        Self(value)
+    }
+
+    pub fn value(&self) -> &str {
+        &self.0
+    }
+}
+
+pub type LabelNode = Node<Label>;
+
+#[derive(Debug, Clone, PartialEq)]
 pub struct Number(f64);
 
 impl Number {
@@ -33,9 +48,29 @@ pub type NumberNode = Node<Number>;
 #[derive(Debug, Clone, PartialEq)]
 pub struct Str(std::string::String);
 
+impl Str {
+    pub fn new(value: String) -> Self {
+        Self(value)
+    }
+
+    pub fn value(&self) -> &str {
+        &self.0
+    }
+}
+
 pub type StrNode = Node<Str>;
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Boolean(bool);
+
+impl Boolean {
+    pub fn new(value: bool) -> Self {
+        Self(value)
+    }
+
+    pub fn value(&self) -> bool {
+        self.0
+    }
+}
 
 pub type BooleanNode = Node<Boolean>;
--- src-rs/oneil_ast/src/parameter.rs
+++ src-rs/oneil_ast/src/parameter.rs
@@ -1,22 +1,49 @@
-use crate::debug_info::TraceLevelNode;
-
-use super::expression::Expr;
-use super::note::Note;
-use super::unit::UnitExpr;
+use crate::{
+    atom::{IdentifierNode, LabelNode},
+    debug_info::TraceLevelNode,
+    expression::ExprNode,
+    node::Node,
+    note::NoteNode,
+    unit::UnitExprNode,
+};
 
 /// A parameter in an Oneil program
 ///
 /// Parameters are used to define the values of variables in the model.
 #[derive(Debug, Clone, PartialEq)]
 pub struct Parameter {
-    pub name: String,
-    pub ident: String,
-    pub value: ParameterValue,
-    pub limits: Option<Limits>,
-    pub is_performance: bool,
+    pub name: LabelNode,
+    pub ident: IdentifierNode,
+    pub value: ParameterValueNode,
+    pub limits: Option<LimitsNode>,
+    pub is_performance: Node<bool>,
     pub trace_level: TraceLevelNode,
-    pub note: Option<Note>,
+    pub note: Option<NoteNode>,
 }
+
+impl Parameter {
+    pub fn new(
+        name: LabelNode,
+        ident: IdentifierNode,
+        value: ParameterValueNode,
+        limits: Option<LimitsNode>,
+        is_performance: Node<bool>,
+        trace_level: TraceLevelNode,
+        note: Option<NoteNode>,
+    ) -> Self {
+        Self {
+            name,
+            ident,
+            value,
+            limits,
+            is_performance,
+            trace_level,
+            note,
+        }
+    }
+}
+
+pub type ParameterNode = Node<Parameter>;
 
 /// A value assigned to a parameter.
 ///
@@ -24,23 +51,63 @@ pub struct Parameter {
 /// that evaluate to different values based on conditions.
 #[derive(Debug, Clone, PartialEq)]
 pub enum ParameterValue {
-    Simple(Expr, Option<UnitExpr>),
-    Piecewise(PiecewiseExpr, Option<UnitExpr>),
+    Simple(ExprNode, Option<UnitExprNode>),
+    Piecewise(PiecewiseExprNode, Option<UnitExprNode>),
 }
+
+impl ParameterValue {
+    pub fn simple(expr: ExprNode, unit: Option<UnitExprNode>) -> Self {
+        Self::Simple(expr, unit)
+    }
+
+    pub fn piecewise(expr: PiecewiseExprNode, unit: Option<UnitExprNode>) -> Self {
+        Self::Piecewise(expr, unit)
+    }
+}
+
+pub type ParameterValueNode = Node<ParameterValue>;
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Limits {
-    Continuous { min: Expr, max: Expr },
-    Discrete { values: Vec<Expr> },
+    Continuous { min: ExprNode, max: ExprNode },
+    Discrete { values: Vec<ExprNode> },
 }
 
-#[derive(Debug, Clone, PartialEq)]
-pub struct PiecewisePart {
-    pub expr: Expr,
-    pub if_expr: Expr,
+impl Limits {
+    pub fn continuous(min: ExprNode, max: ExprNode) -> Self {
+        Self::Continuous { min, max }
+    }
+
+    pub fn discrete(values: Vec<ExprNode>) -> Self {
+        Self::Discrete { values }
+    }
 }
+
+pub type LimitsNode = Node<Limits>;
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct PiecewiseExpr {
-    pub parts: Vec<PiecewisePart>,
+    pub parts: Vec<PiecewisePartNode>,
 }
+
+impl PiecewiseExpr {
+    pub fn new(parts: Vec<PiecewisePartNode>) -> Self {
+        Self { parts }
+    }
+}
+
+pub type PiecewiseExprNode = Node<PiecewiseExpr>;
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct PiecewisePart {
+    pub expr: ExprNode,
+    pub if_expr: ExprNode,
+}
+
+impl PiecewisePart {
+    pub fn new(expr: ExprNode, if_expr: ExprNode) -> Self {
+        Self { expr, if_expr }
+    }
+}
+
+pub type PiecewisePartNode = Node<PiecewisePart>;
