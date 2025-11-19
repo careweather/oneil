930178b68e61ca6c8f8ac30e8aed16481607ede9
Brendon Bown <brendon@careweather.com>
2025-11-19 15:14:52 -0700

    [fuzz] Add more ops

--- src-rs/oneil_eval/fuzz/fuzz_targets/interval_single_op.rs
+++ src-rs/oneil_eval/fuzz/fuzz_targets/interval_single_op.rs
@@ -6,13 +6,38 @@ use libfuzzer_sys::{arbitrary, fuzz_target};
 
 #[derive(Debug, Clone, PartialEq, arbitrary::Arbitrary)]
 enum FuzzData {
-    Add(Interval, Interval),
+    Add { lhs: Interval, rhs: Interval },
+    Div { lhs: Interval, rhs: Interval },
+    Mul { lhs: Interval, rhs: Interval },
+    Neg { val: Interval },
+    Pow { base: Interval, exponent: Interval },
+    Sub { lhs: Interval, rhs: Interval },
 }
 
 fuzz_target!(|data: FuzzData| {
     match data {
-        FuzzData::Add(lhs, rhs) => {
+        FuzzData::Add { lhs, rhs } => {
             let result = lhs + rhs;
+            assert!(result.is_valid());
+        }
+        FuzzData::Div { lhs, rhs } => {
+            let result = lhs / rhs;
+            assert!(result.is_valid());
+        }
+        FuzzData::Mul { lhs, rhs } => {
+            let result = lhs * rhs;
+            assert!(result.is_valid());
+        }
+        FuzzData::Neg { val } => {
+            let result = -val;
+            assert!(result.is_valid());
+        }
+        FuzzData::Pow { base, exponent } => {
+            let result = base.pow(&exponent);
+            assert!(result.is_valid());
+        }
+        FuzzData::Sub { lhs, rhs } => {
+            let result = lhs - rhs;
             assert!(result.is_valid());
         }
     }
