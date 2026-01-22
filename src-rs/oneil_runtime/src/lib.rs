7a9c085accedbfb51c472e325e8eec89c0a8b9df
Brendon Bown <brendon@careweather.com>
2026-01-22 16:34:08 -0700

    [runtime] Init runtime crate

---
+++ src-rs/oneil_runtime/src/lib.rs
@@ -0,0 +1,8 @@
+#![cfg_attr(doc, doc = include_str!("../README.md"))]
+//! Runtime for the Oneil programming language
+
+#![allow(clippy::pedantic)]
+// TODO: remove this once the code is cleaned up
+
+mod cache;
+mod runtime;
