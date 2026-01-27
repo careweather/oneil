c8def61901a975bd1d148bf08ac450ce13b6c58c
Brendon Bown <brendon@careweather.com>
2026-01-27 12:58:42 -0700

    [cache] Use IndexMap for cache

--- src-rs/oneil_runtime/src/cache/ast.rs
+++ src-rs/oneil_runtime/src/cache/ast.rs
@@ -1,40 +1,64 @@
-use std::{collections::HashMap, path::PathBuf};
+use std::path::PathBuf;
 
+use indexmap::IndexMap;
 use oneil_ast as ast;
 use oneil_shared::error::OneilError;
 
 pub struct AstCache {
-    asts: HashMap<PathBuf, Result<ast::ModelNode, Vec<OneilError>>>,
+    asts: IndexMap<PathBuf, ast::ModelNode>,
+    errors: IndexMap<PathBuf, Vec<OneilError>>,
 }
 
 impl AstCache {
     pub fn new() -> Self {
         Self {
-            asts: HashMap::new(),
+            asts: IndexMap::new(),
+            errors: IndexMap::new(),
         }
     }
 
     pub fn insert_ast(&mut self, path: PathBuf, ast: ast::ModelNode) -> &ast::ModelNode {
-        self.asts.insert(path.clone(), Ok(ast));
+        self.asts.insert(path.clone(), ast);
 
-        self.asts
+        &self
+            .asts
             .get(&path)
             .expect("ast should be in cache after insertion")
-            .as_ref()
-            .expect("ast should exist")
     }
 
     pub fn insert_errors(&mut self, path: PathBuf, errors: Vec<OneilError>) -> &[OneilError] {
-        self.asts.insert(path.clone(), Err(errors));
+        self.errors.insert(path.clone(), errors);
 
-        self.asts
+        &self
+            .errors
             .get(&path)
             .expect("errors should be in cache after insertion")
-            .as_ref()
-            .expect_err("should be an error result")
     }
 
-    pub fn get(&self, path: &PathBuf) -> Option<Result<&ast::ModelNode, &Vec<OneilError>>> {
-        self.asts.get(path).map(|result| result.as_ref())
+    pub fn get_result(&self, path: &PathBuf) -> Option<Result<&ast::ModelNode, &Vec<OneilError>>> {
+        self.errors
+            .get(path)
+            .map(|errors| Err(errors.as_ref()))
+            .or_else(|| self.asts.get(path).map(Ok))
+    }
+
+    pub fn contains_result(&self, path: &PathBuf) -> bool {
+        self.get_result(path).is_some()
+    }
+
+    pub fn get_ast(&self, path: &PathBuf) -> Option<&ast::ModelNode> {
+        self.asts.get(path)
+    }
+
+    pub fn contains_ast(&self, path: &PathBuf) -> bool {
+        self.get_ast(path).is_some()
+    }
+
+    pub fn get_errors(&self, path: &PathBuf) -> Option<&Vec<OneilError>> {
+        self.errors.get(path)
+    }
+
+    pub fn contains_errors(&self, path: &PathBuf) -> bool {
+        self.get_errors(path).is_some()
     }
 }
