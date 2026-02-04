e29e87107f559d86a3eb3c22d5801cec3be84230
Brendon Bown <brendon@careweather.com>
2026-02-04 10:46:26 -0700

    [runtime] Add eval cache

--- src-rs/oneil_runtime/src/cache/mod.rs
+++ src-rs/oneil_runtime/src/cache/mod.rs
@@ -1,9 +1,11 @@
-//! Caches for source, AST, and IR used by the runtime.
+//! Caches for source, AST, IR, and evaluation used by the runtime.
 
 mod ast;
+mod eval;
 mod ir;
 mod source;
 
 pub use ast::AstCache;
+pub use eval::EvalCache;
 pub use ir::IrCache;
 pub use source::SourceCache;
