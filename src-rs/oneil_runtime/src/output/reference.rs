6cb4195455c55ba1b2cd395385ab5b1c211eceb9
Brendon Bown <brendon@careweather.com>
2026-02-09 13:28:08 -0700

    [runtime] Update how IR references work

--- src-rs/oneil_runtime/src/output/reference.rs
+++ src-rs/oneil_runtime/src/output/reference.rs
@@ -10,7 +10,7 @@ use std::path::Path;
 use indexmap::IndexMap;
 use oneil_eval::output;
 use oneil_ir as ir;
-use oneil_shared::error::OneilError;
+use oneil_shared::{error::OneilError, span::Span};
 
 use crate::{
     cache::{EvalCache, IrCache},
@@ -172,8 +172,23 @@ impl<'result> ModelIrReference<'result> {
     /// Panics if any submodel's reference has not been visited and
     /// added to the IR cache.
     #[must_use]
-    pub fn submodels(&self) -> IndexMap<&'result ir::SubmodelName, &'result ir::SubmodelImport> {
-        self.model.get_submodels().iter().collect()
+    pub fn submodels(
+        &self,
+    ) -> IndexMap<&'result ir::SubmodelName, SubmodelImportReference<'result>> {
+        self.model
+            .get_submodels()
+            .iter()
+            .map(|(name, submodel_import)| {
+                (
+                    name,
+                    SubmodelImportReference::new(
+                        submodel_import,
+                        self.ir_cache,
+                        self.model.get_references(),
+                    ),
+                )
+            })
+            .collect()
     }
 
     /// Returns a map of reference names to their IR model references or resolution errors.
@@ -185,27 +200,15 @@ impl<'result> ModelIrReference<'result> {
     #[must_use]
     pub fn references(
         &self,
-    ) -> IndexMap<&'result ir::ReferenceName, Result<Self, ResolutionErrorReference<'result>>> {
+    ) -> IndexMap<&'result ir::ReferenceName, ReferenceImportReference<'result>> {
         self.model
             .get_references()
             .iter()
-            .map(|(name, ref_import)| {
-                let entry = self
-                    .ir_cache
-                    .get_entry(ref_import.path().as_ref())
-                    .expect("reference should be in cache");
-
-                let result = entry
-                    .as_ref()
-                    .map(|model| Self {
-                        model,
-                        ir_cache: self.ir_cache,
-                    })
-                    .map_err(|resolution_error| {
-                        ResolutionErrorReference::new(resolution_error, self.ir_cache)
-                    });
-
-                (name, result)
+            .map(|(name, reference_import)| {
+                (
+                    name,
+                    ReferenceImportReference::new(reference_import, self.ir_cache),
+                )
             })
             .collect()
     }
@@ -220,6 +223,127 @@ impl<'result> ModelIrReference<'result> {
     #[must_use]
     pub fn tests(&self) -> Vec<&'result ir::Test> {
         self.model.get_tests().values().collect()
+    }
+}
+
+/// A reference to a submodel import within a model.
+#[derive(Debug, Clone, Copy)]
+pub struct SubmodelImportReference<'result> {
+    submodel_import: &'result ir::SubmodelImport,
+    ir_cache: &'result IrCache,
+    references: &'result IndexMap<ir::ReferenceName, ir::ReferenceImport>,
+}
+
+impl<'result> SubmodelImportReference<'result> {
+    /// Creates a new `SubmodelImportReference` for the given submodel import and IR cache.
+    #[must_use]
+    pub const fn new(
+        submodel_import: &'result ir::SubmodelImport,
+        ir_cache: &'result IrCache,
+        references: &'result IndexMap<ir::ReferenceName, ir::ReferenceImport>,
+    ) -> Self {
+        Self {
+            submodel_import,
+            ir_cache,
+            references,
+        }
+    }
+
+    /// Returns the name of the submodel.
+    #[must_use]
+    pub const fn name(&self) -> &'result ir::SubmodelName {
+        self.submodel_import.name()
+    }
+
+    /// Returns the span of the name of the submodel.
+    #[must_use]
+    pub const fn name_span(&self) -> &'result Span {
+        self.submodel_import.name_span()
+    }
+
+    /// Returns the reference name of the submodel.
+    #[must_use]
+    pub const fn reference_name(&self) -> &'result ir::ReferenceName {
+        self.submodel_import.reference_name()
+    }
+
+    /// Returns the reference import of the submodel.
+    #[expect(
+        clippy::missing_panics_doc,
+        reason = "the panic only happens if an internal invariant is violated"
+    )]
+    #[must_use]
+    pub fn reference_import(&self) -> ReferenceImportReference<'result> {
+        let reference_name = self.submodel_import.reference_name();
+        let reference_import = self
+            .references
+            .get(reference_name)
+            .expect("reference should be found");
+
+        ReferenceImportReference::new(reference_import, self.ir_cache)
+    }
+}
+
+/// A reference to a reference import within a model.
+#[derive(Debug, Clone, Copy)]
+pub struct ReferenceImportReference<'result> {
+    reference_import: &'result ir::ReferenceImport,
+    ir_cache: &'result IrCache,
+}
+
+impl<'result> ReferenceImportReference<'result> {
+    /// Creates a new `ReferenceImportReference` for the given reference import and IR cache.
+    #[must_use]
+    pub const fn new(
+        reference_import: &'result ir::ReferenceImport,
+        ir_cache: &'result IrCache,
+    ) -> Self {
+        Self {
+            reference_import,
+            ir_cache,
+        }
+    }
+
+    /// Returns the name of the reference.
+    #[must_use]
+    pub const fn name(&self) -> &'result ir::ReferenceName {
+        self.reference_import.name()
+    }
+
+    /// Returns the span of the name of the reference.
+    #[must_use]
+    pub const fn name_span(&self) -> &'result Span {
+        self.reference_import.name_span()
+    }
+
+    /// Returns the path of the reference.
+    #[must_use]
+    pub const fn path(&self) -> &'result ir::ModelPath {
+        self.reference_import.path()
+    }
+
+    /// Returns the model that this reference imports.
+    ///
+    /// # Errors
+    ///
+    /// Returns a [`ResolutionErrorReference`](ResolutionErrorReference) if the
+    /// model that is referenced has resolution errors.
+    #[expect(
+        clippy::missing_panics_doc,
+        reason = "the panic only happens if an internal invariant is violated"
+    )]
+    pub fn model(&self) -> Result<ModelIrReference<'result>, ResolutionErrorReference<'result>> {
+        let entry = self
+            .ir_cache
+            .get_entry(self.reference_import.path().as_ref())
+            .expect("reference should be in cache");
+        match entry {
+            Ok(model) => Ok(ModelIrReference::new(model, self.ir_cache)),
+            Err(resolution_error) => Err(ResolutionErrorReference::new(
+                resolution_error,
+                self.ir_cache,
+            )),
+        }
     }
 }
 
