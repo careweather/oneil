ca5dd6b6280037f6ef49464c2d371418711b154b
Brendon Bown <brendon@careweather.com>
2026-02-12 14:40:06 -0700

    [runtime] Fix source loading

--- src-rs/oneil_runtime/src/runtime/source.rs
+++ src-rs/oneil_runtime/src/runtime/source.rs
@@ -1,12 +1,12 @@
 //! Source loading for the runtime.
 
-use std::io::Error as IoError;
 use std::path::Path;
+use std::{io::Error as IoError, path::PathBuf};
 
+use oneil_shared::LoadResult;
 use oneil_shared::error::{AsOneilError, OneilError};
 
 use super::Runtime;
-use crate::output;
 
 impl Runtime {
     /// Loads source code from a file.
@@ -18,51 +18,40 @@ impl Runtime {
         clippy::missing_panics_doc,
         reason = "the panic only happens if an internal invariant is violated"
     )]
-    pub fn load_source(
-        &mut self,
-        path: impl AsRef<Path>,
-    ) -> Result<&str, output::error::FileError> {
+    pub fn load_source(&mut self, path: impl AsRef<Path>) -> &LoadResult<String, FileLoadingError> {
         let path = path.as_ref();
 
-        self.watch_paths.insert(path.to_path_buf());
-
         // Read the source code from the file
-        match std::fs::read_to_string(path) {
-            Ok(source) => {
-                self.source_cache.insert(path.to_path_buf(), source);
-                let source = self.source_cache.get(path).expect("it was just inserted");
-
-                Ok(source)
-            }
+        let load_result = match std::fs::read_to_string(path) {
+            Ok(source) => LoadResult::success(source),
             Err(e) => {
-                let error = InternalIoError::new(path, e);
+                let error = FileLoadingError::new(path.to_path_buf(), e);
                 let error = OneilError::from_error(&error, path.to_path_buf());
 
-                self.errors_cache
-                    .insert_file_errors(path.to_path_buf(), vec![error.clone()]);
-
-                Err(output::error::FileError {
-                    error: Box::new(error),
-                })
+                LoadResult::failure(error)
             }
-        }
+        };
+
+        self.source_cache.insert(path.to_path_buf(), load_result);
+
+        self.source_cache.get(path).expect("it was just inserted")
     }
 }
 
 /// Error type for file loading failures.
-pub(super) struct InternalIoError<'a> {
-    path: &'a Path,
+pub(super) struct FileLoadingError {
+    path: PathBuf,
     error: IoError,
 }
 
-impl<'a> InternalIoError<'a> {
+impl FileLoadingError {
     /// Creates a new file error from a path and I/O error.
-    pub const fn new(path: &'a Path, error: IoError) -> Self {
+    pub const fn new(path: PathBuf, error: IoError) -> Self {
         Self { path, error }
     }
 }
 
-impl AsOneilError for InternalIoError<'_> {
+impl AsOneilError for FileLoadingError {
     fn message(&self) -> String {
         format!("couldn't read `{}` - {}", self.path.display(), self.error)
     }
