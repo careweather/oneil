c3b77b1028534ceaba3e23600fcad62a590e5432
Brendon Bown <brendon@careweather.com>
2026-02-16 15:22:49 -0700

    [refactor] Add docs to errors

--- src-rs/oneil_analysis/src/output/error.rs
+++ src-rs/oneil_analysis/src/output/error.rs
@@ -4,22 +4,27 @@ use std::path::PathBuf;
 
 use indexmap::{IndexMap, IndexSet};
 
+/// Accumulated errors encountered while building a dependency or reference tree.
 #[derive(Debug)]
 pub struct TreeErrors {
     errors: IndexMap<PathBuf, TreeModelError>,
 }
 
 impl TreeErrors {
+    /// Creates an empty error collection.
+    #[must_use]
     pub fn empty() -> Self {
         Self {
             errors: IndexMap::new(),
         }
     }
 
+    /// Records a model-level error for the given path.
     pub fn insert_model_error(&mut self, model_path: PathBuf) {
         self.errors.insert(model_path, TreeModelError::ModelError);
     }
 
+    /// Records a parameter-level error for the given model path and parameter name.
     pub fn insert_parameter_error(&mut self, model_path: PathBuf, parameter_name: String) {
         if let Some(model_errors) = self.errors.get_mut(&model_path) {
             model_errors.insert_parameter_error(parameter_name);
@@ -30,6 +35,7 @@ impl TreeErrors {
         }
     }
 
+    /// Merges another `TreeErrors` into this one, combining errors per model path.
     pub fn extend(&mut self, other: Self) {
         for (path, error) in other.errors {
             if let Some(model_errors) = self.errors.get_mut(&path) {
@@ -41,13 +47,20 @@ impl TreeErrors {
     }
 }
 
+/// Errors for a single model when building a tree.
 #[derive(Debug)]
 pub enum TreeModelError {
+    /// The model could not be loaded or evaluated.
     ModelError,
-    ParamErrors { parameters: IndexSet<String> },
+    /// The model loaded but some parameters had errors.
+    ParamErrors {
+        /// Names of parameters that had errors.
+        parameters: IndexSet<String>,
+    },
 }
 
 impl TreeModelError {
+    /// Adds a parameter error to this model error.
     pub fn insert_parameter_error(&mut self, parameter_name: String) {
         match self {
             Self::ModelError => (),
@@ -57,6 +70,7 @@ impl TreeModelError {
         }
     }
 
+    /// Merges another `TreeModelError` into this one.
     pub fn extend(&mut self, other: Self) {
         match (self, other) {
             (Self::ModelError, _) => (),
@@ -73,8 +87,11 @@ impl TreeModelError {
     }
 }
 
+/// Error when looking up a value for a tree node.
 #[derive(Debug)]
 pub enum GetValueError {
+    /// The model was not found or could not be evaluated.
     Model,
+    /// The parameter was not found in the model.
     Parameter,
 }
