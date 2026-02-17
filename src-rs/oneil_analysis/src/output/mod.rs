1488ab3dc2ea42c578fee8a618783912417028e8
Brendon Bown <brendon@careweather.com>
2026-02-17 16:49:56 -0700

    [analysis] Update independents output

--- src-rs/oneil_analysis/src/output/mod.rs
+++ src-rs/oneil_analysis/src/output/mod.rs
@@ -1,7 +1,8 @@
 //! Output types for analysis (dependency graph, trees, errors).
 
 pub mod error;
-
+mod independents;
 mod tree;
 
+pub use independents::Independents;
 pub use tree::{DependencyTreeValue, ReferenceTreeValue, Tree};
