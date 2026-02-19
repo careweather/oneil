fdacddd4f9884fa50fc8464ca56f57832dcafd79
Brendon Bown <brendon@careweather.com>
2026-02-19 08:03:40 -0700

    [refactor] Update features

--- src-rs/oneil/src/lib.rs
+++ src-rs/oneil/src/lib.rs
@@ -6,17 +6,44 @@
     clippy::multiple_crate_versions,
     reason = "this isn't causing problems, and it's going to take time to fix"
 )]
+#![cfg_attr(
+    all(
+        feature = "rust-lib",
+        feature = "python-imports",
+        feature = "python-lib"
+    ),
+    expect(
+        unused_imports,
+        reason = "if all three features are enabled, `oneil_python` will be re-exported twice"
+    )
+)]
 
-pub use oneil_analysis as analysis;
-pub use oneil_ast as ast;
-pub use oneil_builtins as builtins;
-pub use oneil_cli as cli;
-pub use oneil_eval as eval;
-pub use oneil_ir as ir;
-pub use oneil_lsp as lsp;
-pub use oneil_output as output;
-pub use oneil_parser as parser;
-pub use oneil_python as python;
-pub use oneil_resolver as resolver;
-pub use oneil_runtime as runtime;
-pub use oneil_shared as shared;
+#[cfg(feature = "rust-lib")]
+pub use rust_lib::*;
+
+#[cfg(feature = "rust-lib")]
+mod rust_lib {
+    pub use oneil_analysis as analysis;
+    pub use oneil_ast as ast;
+    pub use oneil_builtins as builtins;
+    pub use oneil_cli as cli;
+    pub use oneil_eval as eval;
+    pub use oneil_ir as ir;
+    pub use oneil_lsp as lsp;
+    pub use oneil_output as output;
+    pub use oneil_parser as parser;
+    pub use oneil_resolver as resolver;
+    pub use oneil_runtime as runtime;
+    pub use oneil_shared as shared;
+
+    #[cfg(feature = "python-imports")]
+    pub use oneil_python as python;
+}
+
+#[cfg(feature = "python-lib")]
+pub use python_lib::*;
+
+#[cfg(feature = "python-lib")]
+mod python_lib {
+    pub use oneil_python as python;
+}
